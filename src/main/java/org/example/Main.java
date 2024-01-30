e766312dbae080f8697b2efc4ceeb950c5309fa2
dominik.jakubaszek <dominik.jakubaszek@soflab.pl>
2024-01-30 12:35:22 +0100

    switch case na main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Hudini's library. Register user first");
+
         Library library = new Library();
         UsersDataBase usersDataBase = new UsersDataBase();
-        Logic logic;
+        Logic logic = new Logic(library,usersDataBase);
         usersDataBase.createNewUser(library);
 
         while (true) {
-            logic = new Logic(library,usersDataBase);
+
             usersDataBase.getAllAvailableUser();
             String userSelection = scanner.nextLine();
             if(usersDataBase.checkifUserExist(userSelection)) {
