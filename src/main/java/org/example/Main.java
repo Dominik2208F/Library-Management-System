02465c44731bc76e6ad69d978f1ea2e000ab4327
dominik.jakubaszek <dominik.jakubaszek@soflab.pl>
2024-02-01 22:42:00 +0100

    obsługa wyjątków update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,13 @@ import javax.sound.midi.Soundbank;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Library menagemet system v1");
+        System.out.println("Library menagemet system v1.Loading a default library...");
         LibraryDataBase libraryDataBase= new LibraryDataBase();
         SetUp SetUp = new SetUp(libraryDataBase);
 
         SetUp.createDefaultLibrariesSetUp("Osiedlowa");
         SetUp.createDefaultLibrariesSetUp("Wojewódzka");
-
+        System.out.println("-----------------");
         Flow flow = new Flow(libraryDataBase);
 
         while (true) {
