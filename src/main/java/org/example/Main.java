e8bf25c6d101a17401fc07cc56631510d2396ce3
dominik.jakubaszek <dominik.jakubaszek@soflab.pl>
2024-02-12 01:28:30 +0100

    nowy UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,26 @@ package org.example;
 
 
 import Frames.LibraryManagementFrame;
+import com.formdev.flatlaf.FlatLightLaf;
 import org.example.LibraryManager.LibraryDataBase;
+
+import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
 
     public static void main(String[] args)  {
 
+
+        FlatLightLaf.setup();
+        UIManager.put( "Button.arc", 999 );
+        UIManager.put( "Component.arc", 990 );
+        UIManager.put( "CheckBox.arc", 999 );
+        UIManager.put( "ProgressBar.arc", 999 );
+        UIManager.put( "TextComponent.arc" , 999 );
+        UIManager.put( "ScrollBar.thumbArc", 999 );
+        UIManager.put( "ScrollBar.thumbInsets", new Insets( 2, 2, 2, 2 ) );
 
         LibraryDataBase libraryDataBase = new LibraryDataBase();
         SetUp SetUp = new SetUp(libraryDataBase);
@@ -16,6 +30,7 @@ public class Main {
 
         new Flow(libraryDataBase);
         new LibraryManagementFrame(libraryDataBase);
+
 
     }
 }
